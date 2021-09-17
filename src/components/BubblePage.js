99cad4b4ff3ed524c372ab78e41a4862809586a0
Joe Escobedo <joe_escob1023@live.com>
2021-09-17 15:14:48 -0700

    not able to load my get request to bubblePage

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -2,26 +2,51 @@ import React, { useEffect, useState } from "react";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-import fetchColorService from '../services/fetchColorService';
+import fetchColorService from "../services/fetchColorService";
+import axios from "axios";
+import { useHistory, useParams } from "react-router";
 
 const BubblePage = () => {
   const [colors, setColors] = useState([]);
   const [editing, setEditing] = useState(false);
+  const { id } = useParams;
+  const { push } = useHistory;
+
+  useEffect(() => {
+    new fetchColorService();
+  }, []);
 
   const toggleEdit = (value) => {
     setEditing(value);
   };
 
   const saveEdit = (editColor) => {
+    axios
+      .put(`http://localhost:5000/api/colors/${id}`, editColor)
+      .then((res) => {
+        push(`/colors/${id}`);
+        setColors(res.data);
+      });
   };
 
   const deleteColor = (colorToDelete) => {
+    axios
+      .delete(`http://localhost:5000/api/colors/${colorToDelete}`)
+      .then((res) => {
+        setColors(res.data);
+        push("/colors");
+      });
   };
-
   return (
     <div className="container">
-      <ColorList colors={colors} editing={editing} toggleEdit={toggleEdit} saveEdit={saveEdit} deleteColor={deleteColor}/>
-      <Bubbles colors={colors}/>
+      <ColorList
+        colors={colors}
+        editing={editing}
+        toggleEdit={toggleEdit}
+        saveEdit={saveEdit}
+        deleteColor={deleteColor}
+      />
+      <Bubbles colors={colors} />
     </div>
   );
 };
--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,26 @@
-import axiosWithAuth from '../helpers/axiosWithAuth';
+import { Component } from "react";
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
-const fetchColorService = () => {
-    
+class fetchColorService extends Component {
+  state = {
+    colors: [],
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    axiosWithAuth()
+      .get("/colors")
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          colors: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 }
 
 export default fetchColorService;
